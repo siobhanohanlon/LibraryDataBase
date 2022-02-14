3bae05f18e3a196d04ae518288eaba68a3c396ac
Siobhán O'Hanlon <siobhanhanlon@outlook.com>
2022-02-14 09:32:49 +0000

    Set up

---
+++ LibraryDataBase/Source.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Struct
+typedef struct
+{
+	long bNum;
+	char bTitle[30];
+	char bAuthor[25];
+	double bPrice;
+}libraryB;
+
+//Declare Funstions
+void initArray();
+
+void main()
+{
+
+}
+
+//Initialise Array
+void initArray()
+{
+	for (int i = 0; i < )
+	{
+
+	}
+}
